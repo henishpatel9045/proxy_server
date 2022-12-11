77a22c048b2b43d28422aababac5db7b9b962a2c
henishpatel9045 <200120107182@git.org.in>
2022-12-11 18:53:33 +0530

    ..

--- proxy/views.py
+++ proxy/views.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,7 +13,7 @@ class WpCheck(APIView):
         url = "https://521a-2402-8100-26a3-d576-5b0f-e682-8794-941f.in.ngrok.io"
         url += "/wp/webhook"
         try:
-            r = requests.post(url, data=data)
+            r = requests.post(url, json=data)
             print(data)
             return Response(status=200)
         except Exception as e:
