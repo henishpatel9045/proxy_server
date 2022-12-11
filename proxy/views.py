5cf6de8b3d9551aeaec16dd92d1ff136288e97ad
henishpatel9045 <200120107182@git.org.in>
2022-12-11 17:53:40 +0530

    ..

--- proxy/views.py
+++ proxy/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 # # WP CHECK
 
 
 def wp_check(request):
-    challenge = request.GET.get("hub.challenge", 0)
-    return HttpResponse(int(challenge), status=200)
+    data = request.POST
+    url = "https://521a-2402-8100-26a3-d576-5b0f-e682-8794-941f.in.ngrok.io"
+    url += "/wp/webhook"
+    try:
+        r = requests.post(url, data=data)
+        print(data)
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=400)
 
