2f4dd5bba5b70d88c9609a98e7a9ec8bea676f81
Henish Patel <200120107182@git.org.in>
2023-03-23 19:52:27 +0530

    get added

--- proxy/views.py
+++ proxy/views.py
@@ -7,6 +7,19 @@ from rest_framework.response import Response
 # # WP CHECK
 
 class WpCheck(APIView):
+    def get(self, request):
+        print(type(request))
+        data = request.data
+        url = "http://3.6.211.9:5100"
+        url += "?hub.challenge="
+        try:
+            url += str(request.GET.get("hub.challenge"))
+            r = requests.get(url)
+            return Response(r.json(), status=200)
+        except Exception as e:
+            return Response(status=400)
+
+
     def post(self, request):
         print(type(request))
         data = request.data
